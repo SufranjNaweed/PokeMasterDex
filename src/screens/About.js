8bfe89d22166a053cf6e126ad5eb1154ce2bcf43
NaweedSufranj <sufranj.naweed@gmail.com>
2020-09-27 18:31:20 +0200

    add : About screens

---
+++ src/screens/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Layout, Text } from '@ui-kitten/components'
+import MainLayout from '../layouts/MainLayout'
+
+const About = () => {
+    return (
+        <MainLayout>
+            <Text category="h1">About</Text>
+        </MainLayout>
+    )
+}
+
+export default About
