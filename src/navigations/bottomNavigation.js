16388379430b9a7ae6297c62c630ba739d10cec5
NaweedSufranj <sufranj.naweed@gmail.com>
2020-09-27 18:32:15 +0200

    update : components

--- src/navigations/bottomNavigation.js
+++ src/navigations/bottomNavigation.js
@@ -1,40 +1,34 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {
-  BottomNavigation,
-  BottomNavigationTab,
-  Layout,
-  Text,
-} from '@ui-kitten/components';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { BottomNavigation, BottomNavigationTab, Icon } from '@ui-kitten/components'
+
+import PokeMasterDex from '../screens/PokeMasterDex'
+import About from '../screens/About'
 
 const {Navigator, Screen} = createBottomTabNavigator();
 
-const UsersScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category="h1">USERS</Text>
-  </Layout>
-);
+const PokeMasterDexIcon = (props) => (
+  <Icon {...props} name='compass-outline' />
+)
 
-const OrdersScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category="h1">ORDERS</Text>
-  </Layout>
-);
+const AboutIcon = (props) => (
+  <Icon {...props} name="info-outline" />
+)
 
 const BottomTabBar = ({navigation, state}) => (
   <BottomNavigation
     selectedIndex={state.index}
     onSelect={(index) => navigation.navigate(state.routeNames[index])}>
-    <BottomNavigationTab title="USERS" />
-    <BottomNavigationTab title="ORDERS" />
+    <BottomNavigationTab title="POKE MASTER DEX" icon={PokeMasterDexIcon} />
+    <BottomNavigationTab title="ABOUT" icon={AboutIcon} />
   </BottomNavigation>
 );
 
 const TabNavigator = () => (
   <Navigator tabBar={(props) => <BottomTabBar {...props} />}>
-    <Screen name="Users" component={UsersScreen} />
-    <Screen name="Orders" component={OrdersScreen} />
+    <Screen name="PokeMasterDex" component={PokeMasterDex} />
+    <Screen name="About" component={About} />
   </Navigator>
 );
 
