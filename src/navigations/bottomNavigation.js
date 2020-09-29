2aceba6d385abad955ab9132a79212b2b955a5f2
SufranjNaweed <sufranj.naweed@gmail.com>
2020-09-29 22:51:22 +0200

    BIG UPDATE : bottomNavigation

--- src/navigations/bottomNavigation.js
+++ src/navigations/bottomNavigation.js
@@ -1,41 +1,83 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { BottomNavigation, BottomNavigationTab, Icon } from '@ui-kitten/components'
 
-import PokeMasterDex from '../screens/PokeMasterDex'
+import PokemonDetails from '../screens/PokemonDetails'
+
+// Screens - Tabs
+import SearchPokemon from '../screens/SearchPokemon'
+import SavedPokemon from '../screens/SavedPokemon'
 import About from '../screens/About'
 
-const {Navigator, Screen} = createBottomTabNavigator();
+// Screens - PokeDex Stack
+import GenerationPokemon from '../screens/PokemonGenerations'
+import PokemonsPerGeneration from '../screens/PokemonsPerGeneration'
 
-const PokeMasterDexIcon = (props) => (
-  <Icon {...props} name='compass-outline' />
-)
+// STACKS
 
-const AboutIcon = (props) => (
-  <Icon {...props} name="info-outline" />
-)
+// Pokedex
+const PokeDexStack = createStackNavigator();
 
-const BottomTabBar = ({navigation, state}) => (
-  <BottomNavigation
-    selectedIndex={state.index}
-    onSelect={(index) => navigation.navigate(state.routeNames[index])}>
-    <BottomNavigationTab title="POKE MASTER DEX" icon={PokeMasterDexIcon} />
-    <BottomNavigationTab title="ABOUT" icon={AboutIcon} />
-  </BottomNavigation>
-);
+function PokeDexStackScreen() {
+  return (
+    <PokeDexStack.Navigator>
+      <PokeDexStack.Screen name="PokemonGenerations" component={GenerationPokemon} />
+      <PokeDexStack.Screen name="PokemonsPerGeneration" component={PokemonsPerGeneration} />
+      <PokeDexStack.Screen name="PokemonDetails" component={PokemonDetails}/>
+    </PokeDexStack.Navigator>
+  );
+}
 
-const TabNavigator = () => (
-  <Navigator tabBar={(props) => <BottomTabBar {...props} />}>
-    <Screen name="PokeMasterDex" component={PokeMasterDex} />
-    <Screen name="About" component={About} />
-  </Navigator>
-);
+// Search Pokemon
+const SearchStack = createStackNavigator();
 
-const AppNavigator = () => (
-  <NavigationContainer>
-    <TabNavigator />
-  </NavigationContainer>
-);
+function SearchStackScreen() {
+  return (
+    <SearchStack.Navigator>
+      <SearchStack.Screen name="Search" component={SearchPokemon} />
+      <SearchStack.Screen name="PokemonDetails" component={PokemonDetails}/>
+    </SearchStack.Navigator>
+  ) 
+}
 
-export default AppNavigator
+// Saved Pokemon
+const SavedPokemonStack = createStackNavigator()
+
+function SavedPokemonScreen(){
+  return (
+    <SavedPokemonStack.Navigator>
+      <SavedPokemonStack.Screen name="Saved" component={SavedPokemon} />
+      <SavedPokemonStack.Screen name="PokemonDetails" component={PokemonDetails} />
+    </SavedPokemonStack.Navigator>
+  )
+}
+
+// About
+const AboutStack = createStackNavigator()
+
+function AboutScreen(){
+  return(
+    <AboutStack.Navigator>
+      <AboutStack.Screen name="About" component={About} />
+    </AboutStack.Navigator>
+  )
+}
+
+// TABS
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+  return(
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="POKEDEX" component={PokeDexStackScreen} />
+        <Tab.Screen name="SEARCH" component={SearchStackScreen} />
+        <Tab.Screen name="SAVED" component={SavedPokemonScreen} />
+        <Tab.Screen name="ABOUT" component={AboutScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default TabNavigation
