d8ecd741c1751086728c830c474b3c5b389b3920
SufranjNaweed <sufranj.naweed@gmail.com>
2020-09-30 23:54:12 +0200

    add : Context to App.js

--- App.js
+++ App.js
@@ -8,15 +8,15 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {
-  SafeAreaView,
   StatusBar,
   View,
 } from 'react-native';
 
 import * as eva from '@eva-design/eva';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { ApplicationProvider, IconRegistry, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { default as theme } from './src/configs/custom-theme.json';
+import PokemonContextProvider from './src/contexts/pokemon'
 import TabNavigation from './src/navigations/bottomNavigation';
 
 const App = () => {
@@ -25,8 +25,10 @@ const App = () => {
       <StatusBar backgroundColor="#FFF" barStyle="dark-content" />
       <View style={{flex : 1 , backgroundColor : 'red'}}>
           <IconRegistry icons={EvaIconsPack} />
-          <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>  
-            <TabNavigation /> 
+          <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
+            <PokemonContextProvider>
+              <TabNavigation /> 
+            </PokemonContextProvider>
           </ApplicationProvider>
       </View>
     </>
