c33c46a173615a485cce616627c4117318758d6a
SufranjNaweed <sufranj.naweed@gmail.com>
2020-10-01 06:35:09 +0200

    add : PokeCard

---
+++ src/components/PokeCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, Image} from 'react-native'
+
+
+const PokeCard = () => {
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default PokeCard
